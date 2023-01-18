785e27a11abe50a75f3c3838188d6ba5b089e930
PiiYukana01 <104289571+PiiYukana01@users.noreply.github.com>
2023-01-18 20:55:40 +0700

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "Trời Giấu Trời Mang Đi",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Giấc Mơ Rất Thơ",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tình Thật Tình Mơ",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Đắp Mộ Magaming",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Thương Cho Tấm Thân",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Thà Yêu Lấy Một Người Bình Thường",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Ngàn Câu Hứa",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Nắm Tay Nhau",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Sợ Ngày Mai Em Đi Mất",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Trót Trao Duyên",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Ngày Thơ ẤU",
+        artist: "Nguyễn Hoàng Dũng",
+        img: "9",
+        src: "9"
+    },
+];
